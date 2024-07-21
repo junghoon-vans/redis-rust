4b392cc0d5beb753f6156780bb9b16e864d28068
Junghoon Ban <junghoon.ban@gmail.com>
2024-07-21 16:00:18 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(mut stream) => {
+            Ok((mut stream, _)) => {
                 println!("accepted new connection");
 
-                let mut buf = [0; 512];
-                loop {
-                    match stream.read(&mut buf) {
-                        Ok(0) => break,
-                        Err(e) => {
-                            println!("An error occurred while reading: {}:", e);
-                            break;
+                tokio::spawn(async move {
+                    let mut buf = [0; 512];
+                    loop {
+                        match stream.read(&mut buf).await {
+                            Ok(0) => break,
+                            Err(e) => {
+                                println!("An error occurred while reading: {}:", e);
+                                break;
+                            }
+                            _ => {},
                         }
-                        _ => {},
-                    }
 
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+                        stream.write(b"+PONG\r\n").await.unwrap();
+                    }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
