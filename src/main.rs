854aacc3755bc05a6d536fa4445a1abfb9766e4f
Junghoon Ban <junghoon.ban@gmail.com>
2024-07-22 00:34:43 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+use stream::{StreamHandler, Value};
+mod stream;
 
 #[tokio::main]
 async fn main() {
@@ -9,23 +11,11 @@ async fn main() {
         let stream = listener.accept().await;
 
         match stream {
-            Ok((mut stream, _)) => {
+            Ok((stream, _)) => {
                 println!("accepted new connection");
 
                 tokio::spawn(async move {
-                    let mut buf = [0; 512];
-                    loop {
-                        match stream.read(&mut buf).await {
-                            Ok(0) => break,
-                            Err(e) => {
-                                println!("An error occurred while reading: {}:", e);
-                                break;
-                            }
-                            _ => {},
-                        }
-
-                        stream.write(b"+PONG\r\n").await.unwrap();
-                    }
+                    handle_conn(stream).await;
                 });
             }
             Err(e) => {
@@ -34,3 +24,45 @@ async fn main() {
         }
     }
 }
+
+async fn handle_conn(stream: TcpStream) {
+    let mut handler = StreamHandler::new(stream);
+    loop {
+        let command = handler.read_request().await.unwrap();
+
+        let response = if let Some(command) = command {
+            let (cmd, args) = extract_command(command).unwrap();
+            match cmd.as_str() {
+                "PING" => Value::SimpleString("PONG".to_string()),
+                "ECHO" => args.first().unwrap().clone(),
+                _ => Value::SimpleString("OK".to_string()),
+            }
+        } else {
+            break;
+        };
+
+        println!("response: {:?}", response);
+        handler.write_response(response).await.unwrap();
+    }
+}
+
+fn extract_command(value: Value) -> Result<(String, Vec<Value>), anyhow::Error> {
+    match value {
+        Value::Array(a) => {
+            Ok((
+                unpack_bulk_str(a.first().unwrap().clone())?,
+                a.into_iter().skip(1).collect(),
+            ))
+        }
+        _ => Err(anyhow::anyhow!("Invalid command format")),
+    }
+}
+
+fn unpack_bulk_str(value: Value) -> Result<String, anyhow::Error> {
+    match value {
+        Value::BulkString(s) => Ok(s),
+        _ => Err(anyhow::anyhow!("Expected command to be a bulk string")),
+    }
+}
+
+
